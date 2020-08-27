c4d17cd094e22cf52c6c96f4a37052396b525a4d
Tero Keso <29271631+TeroKeso@users.noreply.github.com>
2020-08-27 11:51:01 +0300

    Tero Testaa

---
+++ asennus.ps1
@@ -0,0 +1,12 @@
+winget install Microsoft.PowerShell
+
+
+winget install Microsoft.VisualStudioCode  
+winget install Microsoft.WindowsTerminal 
+winget install git.git
+code --install-extension ms-vscode.powershell
+
+
+
+
+##
